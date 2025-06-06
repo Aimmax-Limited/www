cfc60d78df1a93566886e31b40792a6eae6c7f21
thatboyreegan <reaganronald491@gmail.com>
2025-06-06 23:29:24 +0300

    sashing my components

---
+++ app/components/navbar.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router';
+import { Button } from './ui/button';
+import { Sheet, SheetTrigger, SheetContent } from './ui/sheet';
+import ShinyText from './reactbits/shiny-text';
+import StarBorder from './reactbits/star-border';
+
+const links = [{
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Services",
+    path: "/",
+  },
+  {
+    name: "Portfolio",
+    path: "/",
+  },
+  {
+    name: "About us",
+    path: "/about",
+  },]
+export default function Navbar() {
+  return (
+    <div className='flex justify-between items-center bg-red-200'>
+      <Logo/>
+      <div className='hidden md:flex pe-0 md:pe-2 lg:pe-10 xl:pe-20 2xl:pe-32 3xl:pe-40 gap-0.5 md:gap-2 lg:gap-10 xl:gap-10 2xl:gap-12'>
+        {links.map((link) => (
+          <NAvLink key={link.name} name={link.name} path={link.path}/>
+        ))}
+        <StarBorder><Link to={'/contact us'}><ShinyText text='Contact us' className='group-hover:text-teal-600 hover:bg-slate-900'/></Link></StarBorder>
+          
+        
+      </div>
+      <div className='block md:hidden pe-4'>
+        <SheetBar/>
+      </div>
+      
+    </div>
+  );
+}
+
+
+function Logo() {
+  return (
+    <div className='flex items-center'>
+      <img src="./logo-light.svg" alt="Logo" width={90} />
+      <div className='text-black font-mono text-center'><p>Aimmax</p>
+      <p>Limited</p></div>
+      
+    </div>
+  )
+}
+
+function NAvLink ({name, path, ...props}: {name: string; path: string} & React.ComponentProps<'button'>) {
+  return (
+    <Button variant="link">
+      <NavLink to={path} className={({isActive, isPending}) => isActive ? 'text-black' : isPending ? 'text-green-400' : 'text-cyan-500'} preventScrollReset>
+        {name}
+      </NavLink>
+    </Button>
+  )
+
+}
+
+function SheetBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <img src="./align-right.svg" alt="Menu Icon" width={60} />
+      </SheetTrigger>
+      <SheetContent className=''>
+        {links.map((link) => (
+          <NAvLink key={link.name}
+          name={link.name}
+          path={link.path}
+          onClick={() => setIsOpen(false)}/>
+        ))}
+      </SheetContent>
+    </Sheet>
+  )
+}
