3f58423946a61b9bff13d1eba869a23dfdaa5756
Brian Kim <bryo.kim1@gmail.com>
2025-06-07 22:46:05 +0300

    Formatting

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router';
-import { Button } from './ui/button';
-import { Sheet, SheetTrigger, SheetContent } from './ui/sheet';
-import ShinyText from './reactbits/shiny-text';
-import StarBorder from './reactbits/star-border';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router";
+import ShinyText from "./reactbits/shiny-text";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
-const links = [{
+const links = [
+  {
     name: "Home",
     path: "/",
   },
@@ -20,48 +20,69 @@ const links = [{
   {
     name: "About us",
     path: "/about",
-  },]
+  },
+];
 export default function Navbar() {
   return (
-    <div className='flex justify-between items-center rounded-2xl'>
-      <Logo/>
-      <div className='hidden md:flex pe-0 md:pe-2 lg:pe-10 xl:pe-20 2xl:pe-32 3xl:pe-40 gap-0.5 md:gap-2 lg:gap-10 xl:gap-10 2xl:gap-12'>
+    <div className="flex justify-between items-center rounded-2xl">
+      <Logo />
+      <div className="hidden md:flex pe-0 md:pe-2 lg:pe-10 xl:pe-20 2xl:pe-32 3xl:pe-40 gap-0.5 md:gap-2 lg:gap-10 xl:gap-10 2xl:gap-12">
         {links.map((link) => (
-          <NAvLink key={link.name} name={link.name} path={link.path}/>
+          <NAvLink key={link.name} name={link.name} path={link.path} />
         ))}
-        <Button variant={'outline'}className="group bg-[#0A192F] hover:bg-slate-900 border-0 ring-0 outline-0 text-center text-base"><Link to={'/contact us'}><ShinyText text='Contact us' className='group-hover:text-teal-600 hover:bg-slate-900'/></Link></Button>
-          
-        
+        <Button
+          variant={"outline"}
+          className="group bg-[#0A192F] hover:bg-slate-900 border-0 ring-0 outline-0 text-center text-base"
+        >
+          <Link to={"/contact us"}>
+            <ShinyText
+              text="Contact us"
+              className="group-hover:text-teal-600 hover:bg-slate-900"
+            />
+          </Link>
+        </Button>
       </div>
-      <div className='block md:hidden pe-4'>
-        <SheetBar/>
+      <div className="block md:hidden pe-4">
+        <SheetBar />
       </div>
-      
     </div>
   );
 }
 
-
 function Logo() {
   return (
-    <div className='flex items-center'>
+    <div className="flex items-center">
       <img src="./logo-light.svg" alt="Logo" width={90} />
-      <div className='text-black font-mono text-center'><p>Aimmax</p>
-      <p>Limited</p></div>
-      
+      <div className="text-black font-mono text-center">
+        <p>Aimmax</p>
+        <p>Limited</p>
+      </div>
     </div>
-  )
+  );
 }
 
-function NAvLink ({name, path, ...props}: {name: string; path: string} & React.ComponentProps<'button'>) {
+function NAvLink({
+  name,
+  path,
+  ...props
+}: { name: string; path: string } & React.ComponentProps<"button">) {
   return (
     <Button variant="link">
-      <NavLink to={path} className={({isActive, isPending}) => isActive ? 'text-black' : isPending ? 'text-green-400' : 'text-cyan-500'} preventScrollReset>
+      <NavLink
+        to={path}
+        className={({ isActive, isPending }) =>
+          isActive
+            ? "text-black"
+            : isPending
+            ? "text-green-400"
+            : "text-cyan-500"
+        }
+        preventScrollReset
+      >
         {name}
       </NavLink>
     </Button>
-  )
-
+  );
 }
 
 function SheetBar() {
@@ -72,15 +93,27 @@ function SheetBar() {
       <SheetTrigger asChild>
         <img src="./align-right.svg" alt="Menu Icon" width={60} />
       </SheetTrigger>
-      <SheetContent className='flex flex-col items-center'>
+      <SheetContent className="flex flex-col items-center">
         {links.map((link) => (
-          <NAvLink key={link.name}
-          name={link.name}
-          path={link.path}
-          onClick={() => setIsOpen(false)}/>
+          <NAvLink
+            key={link.name}
+            name={link.name}
+            path={link.path}
+            onClick={() => setIsOpen(false)}
+          />
         ))}
-        <Button variant={'outline'}className="group bg-[#0A192F] hover:bg-slate-900 border-0 ring-0 outline-0 text-center text-base"><Link to={'/contact us'}><ShinyText text='Contact us' className='group-hover:text-teal-600 hover:bg-slate-900'/></Link></Button>
+        <Button
+          variant={"outline"}
+          className="group bg-[#0A192F] hover:bg-slate-900 border-0 ring-0 outline-0 text-center text-base"
+        >
+          <Link to={"/contact us"}>
+            <ShinyText
+              text="Contact us"
+              className="group-hover:text-teal-600 hover:bg-slate-900"
+            />
+          </Link>
+        </Button>
       </SheetContent>
     </Sheet>
-  )
+  );
 }
