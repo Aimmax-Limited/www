1d2fb0a70a8ad7986384187bdffbaa68d66ffe04
Brian Kim <bryo.kim1@gmail.com>
2025-06-08 14:22:24 +0300

    add a tentative page for home path

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"),
-    route('about', 'routes/about.tsx'),
-    route('contactUs', 'routes/contactUs.tsx')
+export default [
+  index("routes/coming-soon.tsx"),
+  route("dev-home", "routes/home.tsx"),
+  route("dev-about", "routes/about.tsx"),
+  route("dev-contactUs", "routes/contactUs.tsx"),
 ] satisfies RouteConfig;
