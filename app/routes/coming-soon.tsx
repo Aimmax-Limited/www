10ff28f6d29d0a3c8a30bd129f2137e4dfe73b1c
Brian Kim <bryo.kim1@gmail.com>
2025-06-09 21:41:57 +0300

    fix: margin exceeding screen height on smaller devices

--- app/routes/coming-soon.tsx
+++ app/routes/coming-soon.tsx
@@ -1,12 +1,25 @@
 import { Mail, PhoneCall } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ShinyText from "~/components/reactbits/shiny-text";
+import { cn } from "~/lib/utils";
 
 export default function ComingSoon() {
+  const BP_HEIGHT = 800;
+
+  const [height_, setHeight] = useState(0);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHeight(window.innerHeight);
+    }
+  }, []);
+
   return (
-    <div className="h-dvh bg-[#0A192F] text-white font-clashdisplay">
-      <div className="h-dvh max-w-screen-md md:max-w-screen-lg mx-auto flex flex-col items-center">
-        <div className="mt-28">
+    <div className="min-h-dvh bg-[#0A192F] text-white font-clashdisplay">
+      <div className="h-full max-w-screen-md md:max-w-screen-lg mx-auto flex flex-col items-center">
+        <div
+          className={cn(height_ >= BP_HEIGHT ? "mt-28" : "mt-14", "md:mt-28")}
+        >
           <h1
             className="font-semibold text-center text-4xl md:text-6xl lg:text-8xl uppercase"
             style={{ wordSpacing: "30px" }}
@@ -22,7 +35,12 @@ export default function ComingSoon() {
           </p>
         </div>
 
-        <div className="max-w-screen-ms mt-36 flex flex-col justify-between items-center gap-3 font-satoshi">
+        <div
+          className={cn(
+            height_ >= BP_HEIGHT ? "mt-28" : " mt-10",
+            "max-w-screen-ms md:mt-36 flex flex-col justify-between items-center gap-3 font-satoshi"
+          )}
+        >
           <h2 className="font-clashdisplay font-medium text-4xl">Contact Us</h2>
 
           <div className="flex items-center gap-2">
@@ -43,11 +61,16 @@ export default function ComingSoon() {
           </Link>
         </div>
 
-        <div className="mt-28 text-xl">
+        <div
+          className={cn(
+            height_ >= BP_HEIGHT ? "mt-28" : "mt-10",
+            "md:mt-28 text-xl"
+          )}
+        >
           <ShinyText text="Full website coming soon"></ShinyText>
         </div>
 
-        <div className="min-h-10 bg-slate-800 fixed bottom-0 left-0 w-full text-center font-satoshi flex justify-center items-center">
+        <div className="min-h-10 bg-slate-800 fixed bottom-0 left-0 w-full text-center font-satoshi flex justify-center items-center transition-opacity duration-400">
           <div className="max-w-screen-xl mx-auto text-white text-sm md:text-base lg:text-lg ">
             © 2025 Aimmax Company Ltd. All rights reserved.
           </div>
