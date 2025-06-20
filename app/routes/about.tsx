e36d40a4493c26cdc98a0188cd6ab442d3622f2c
Brian Kim <bryo.kim1@gmail.com>
2025-06-21 00:08:12 +0300

    Update about us page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,5 +1,6 @@
 import Footer from "~/components/footer";
 import Navbar from "~/components/navbar";
+import SpotlightCard from "~/components/reactbits/spotlight-card";
 
 export default function About() {
   return (
@@ -15,11 +16,28 @@ export default function About() {
       </div>
 
       <div className="relative z-20 bg-white">
-        <div className="py-10 px-4 xl:px-0 max-w-screen-xl mx-auto">
+        <div className="py-10 xl:py-14 px-4 md:px-8 xl:px-0 max-w-screen-xl mx-auto grid md:grid-cols-2 gap-10">
+          {statements.map((stmt, index) => (
+            <SpotlightCard
+              className="text-center bg-white/10 rounded-lg hover:bg-white backdrop-blur-xl border-0 shadow-[0_6px_15px_rgba(0,0,0,0.3)]"
+              spotlightColor="rgba(180, 254, 0, 0)"
+              key={index}
+            >
+              <h3 className="font-fredoka font-medium lg:font-normal text-2xl md:text-3xl lg:text-[40px] pt-5 md:pt-0 mb-8">
+                {stmt.name}
+              </h3>
+              <p className="font-satoshi font-medium text-[17px] text-slate-800 px-2 md:px-0">
+                {stmt.description}
+              </p>
+            </SpotlightCard>
+          ))}
+        </div>
+
+        <div className="py-10 xl:py-14 px-4 md:px-8 xl:px-0 max-w-screen-xl mx-auto border-t border-slate-300">
           <h2 className="font-fredoka font-medium lg:font-normal text-center text-2xl md:text-3xl lg:text-[40px] mb-8">
             Company Profile
           </h2>
-          <p className="text-lg font-satoshi font-medium">
+          <p className="text-lg font-satoshi font-medium text-slate-800 text-justify">
             Aimmax Company Ltd, a Kenyan firm, consists of experienced
             professionals with diverse expertise. The company has evolved from a
             sole proprietorship to a fully incorporated entity, specializing in
@@ -39,6 +57,27 @@ export default function About() {
             partnerships in the industry.
           </p>
         </div>
+
+        <div className="py-10 xl:py-14 px-4 md:px-8 xl:px-0 max-w-screen-xl mx-auto border-t border-slate-300">
+          <h2 className="font-fredoka font-medium lg:font-normal text-center text-2xl md:text-3xl lg:text-[40px] mb-8">
+            Our Strategy
+          </h2>
+          <p className="text-lg font-satoshi font-medium">
+            Our strategy is to build along-term Asset management solutions by
+            leveraging the strength of our management team and staff
+            capabilities. We will focus, drive and optimize our business to
+            embrace new technology to add value to our customers.
+            <br /> <br />
+            Our Strategic Priorities are:
+          </p>
+          <ul className="list-decimal ps-5 lg:ps-10 pt-3 font-satoshi font-medium">
+            {StrategicPriorities.map((priority, index) => (
+              <li className="my-3" key={index}>
+                {priority}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <div className="relative z-20 bg-white">
@@ -47,3 +86,25 @@ export default function About() {
     </div>
   );
 }
+
+const statements = [
+  {
+    name: "Mission",
+    description:
+      "Our mission is to deliver added value services to our clients, guiding actions and decisions with precision and integrity.",
+  },
+  {
+    name: "Vision",
+    description:
+      "To be the leading Asset Management Company, consistently delivering superior investment performance and innovative solutions that enhance the financial well-being of our clients while adhering to the highest standards·",
+  },
+];
+
+const StrategicPriorities = [
+  "Develop the customer proposition and experience",
+  "Deliver high performance in all needs",
+  "Share skill and experience across the management team, staff and customers",
+  "Build a culture of excellence",
+  "Simplify our structure to unlock value",
+  "Embrace new technology on Assets management and accounting standards",
+];
