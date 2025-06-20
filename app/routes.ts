e680a8ceeab7d23f141908d24b0626dd31bd44ba
Brian Kim <bryo.kim1@gmail.com>
2025-06-20 21:39:08 +0300

    Move home to the index page

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/coming-soon.tsx"),
-  route("dev-home", "routes/home.tsx"),
+  index("routes/home.tsx"),
+  route("coming-soon", "routes/coming-soon.tsx"),
   route("dev-services", "routes/services.tsx"),
   route("dev-about", "routes/about.tsx"),
   route("dev-contactUs", "routes/contactUs.tsx"),
