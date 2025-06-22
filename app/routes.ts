dc95153c98362b988ea18cd30796110dc9bfaba4
Brian Kim <bryo.kim1@gmail.com>
2025-06-23 00:19:56 +0300

    refactor: rename contactUs to contact-us

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,9 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-
   index("routes/home.tsx"),
   route("coming-soon", "routes/coming-soon.tsx"),
   route("dev-services", "routes/services.tsx"),
   route("dev-about", "routes/about.tsx"),
-  route("dev-contactUs", "routes/contactUs.tsx"),
+  route("dev-contact-us", "routes/contact-us.tsx"),
 ] satisfies RouteConfig;
